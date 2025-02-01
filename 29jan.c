ce25d77614a67f682ec7c6c6367583b60f285dbe
kunal12-9 <ktailor515@gmail.com>
2025-02-01 12:58:48 +0530

    Add files via upload

---
+++ 29jan.c
@@ -0,0 +1,54 @@
+q. print country name
+#include<stdio.h>
+int main() {
+    
+    printf("india \nbhutan \ndubai \namerica \nsrilanka");
+    
+    return 0;
+}
+
+q.print movie name
+#include<stdio.h>
+int main() {
+    
+    printf("kgf \npushpa \n3iditos \nbhoolbhulaiya \ngadar");
+    
+    return 0;
+    
+}
+
+
+q. print friends name
+
+#include<stdio.h>
+int main() {
+    
+    printf("kartikey \nnaresh \nanshul \nabhay \npraveen");
+    
+    return 0;
+    
+}
+
+q. print food name
+
+#include<stdio.h>
+int main() {
+    
+    printf("mango \napple \nsamosa \npizza \ncake");
+    
+    return 0;
+    
+}
+
+q.print 5 enemy name 
+
+#include<stdio.h>
+int main() {
+    
+    printf("vijay \nrahul \nabhisek \nyogendra \nvikash");
+    
+    return 0;
+    
+}
+
+
